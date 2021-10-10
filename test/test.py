d0af0cd49eaa83f7d4b96fb3ff5d8fec0129b258
dangola02 <78271298+dangola02@users.noreply.github.com>
2021-10-10 18:05:24 +0600

    Add files via upload

---
+++ test/test.py
@@ -0,0 +1,8 @@
+
+# Just copy paste this to python_news to check by different coins
+
+scrapper.find_news_by_coin('cardano')
+
+scrapper.find_news_by_coin('bitcoin')
+
+scrapper.find_news_by_coin('etherium')
